735fbb999c1c97f981a6415e9ba1b6414ee130dd
Clementho <clementhosk@gmail.com>
2023-08-28 21:18:26 +1000

    Universal styling

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,22 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: 'Space Mono, monospace',
+  },
+});
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
