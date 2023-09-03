b36e54fef4c2e5266b1b68a1644fd5160cde3b52
Demetre <demetre.de.alwis@gmail.com>
2023-09-03 19:54:49 +1000

    Added Comments

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,10 +17,12 @@ function App() {
         <Route path="/marketplace" element={<Marketplace />}></Route>
         <Route path="/user" element={<User />}></Route>
         <Route path="/editprofile" element={<EditProfile />}></Route>
+
+        {/* "id" is can be exchanged for a product id to view said product  */}
         <Route path="/marketplace/:id" element={<ProductDetails />}></Route>
 
         {/* Redirect to homepage when user tries to access non-existent page */}
-        <Route path="*" element={<Navigate to="/"/>} />
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
       <Footer />
     </div>
